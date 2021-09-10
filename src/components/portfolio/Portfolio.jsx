30b6525b812137b41e25c05f0ce30ba7626f47c1
lukatchubabria <lukatchubabria@gmail.com>
2021-09-10 04:34:28 +0400

    add burger menu

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,8 +2,8 @@ import './portfolio.scss'
 
 export default function Portfolio() {
     return (
-        <div className="portfolio">
-            fasdfasdf
+        <div className="portfolio" id="portfolio">
+
         </div>
     )
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,29 @@
-import './topbar.scss'
+import { Mail, Person } from '@material-ui/icons';
+import './topbar.scss';
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
     return (
-        <div className="topbar">
-            
+        <div className={"topbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">MyName.</a>
+                    <div className="itemcontainer">
+                        <Person className="icon"/>
+                        <span>+995 558 755 299</span>
+                    </div>
+                    <div className="itemcontainer">
+                        <Mail className="icon"/>
+                        <span>lukatchubabria@gmail.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
